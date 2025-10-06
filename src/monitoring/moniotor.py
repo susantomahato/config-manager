92813a5035b6ae9105c6b5727d407bc74b950dcf
susantomahato <susantomahato88@gmail.com>
2025-10-06 11:19:36 -0700

    adding scapfoldig class for monitoring

---
+++ src/monitoring/moniotor.py
@@ -0,0 +1,6 @@
+# Class for monitoring and logging system metrics
+class SystemMonitor:
+    pass.
+
+# TODO : Implemention is pending.
+# Can be integrate with exsiting monitoring tool like prometheus, grafana etc.
